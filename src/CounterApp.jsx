740471f14fee57d73451f02cc33315f63462ed67
MariangelyG <mariangelygonzalez90@gmail.com>
2023-09-11 17:50:39 -0300

    "learning react"

---
+++ src/CounterApp.jsx
@@ -0,0 +1,33 @@
+import {useState} from 'react'
+import PropTypes from 'prop-types';
+
+export const CounterApp = ({value})=>{
+
+    const [counter, setCounter] = useState(value)
+
+    const handleAdd = (event) =>{
+        // console.log(event)
+    //   setCounter(counter + 1)
+        setCounter((c)=> c + 1)
+    }
+    const handleSubtract = () => setCounter((c)=> c - 1)
+    const handleReset = () => setCounter((c)=> value)
+
+
+    return(
+        <>
+        <h1>CounterApp</h1>
+        <h2> {counter} </h2>
+
+        <button onClick={ (handleAdd) }> +1 </button>
+        <button onClick={ (handleSubtract) }> -1 </button>
+        <button onClick={ (handleReset) }> Reset </button>
+
+        </>
+        
+        )
+}
+
+CounterApp.prototype = {
+value: PropTypes.number.isRequired,
+}
